42f8824d0033adde96970dd1f6179a9335457e06
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-10-16 22:07:28 +0300

    Entrypoint args updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo n | "$HOME/jelastic/users/authentication/signin" --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" > /dev/null
-"$HOME"/jelastic/"$1"
+"$HOME"/jelastic/"$*"
