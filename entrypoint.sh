a316a21f57e14d64a88a3ebeec3115c9c46d4ceb
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-10-19 18:09:25 +0300

    fix: Path to CLI changed to abs instead of HOME usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-response=$(echo n | "$HOME/jelastic/users/authentication/signin" --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" > /dev/null
-"$HOME"/jelastic/"$*" | sed '/./{H;$!d} ; x ; s/^[^{]*//g' | sed '/./{H;$!d} ; x ; s/[^}]*$//g' | jq
+response=$(echo n | /cli/jelastic/users/authentication/signin --login "$JELASTIC_USERNAME" --password "$JELASTIC_PASSWORD" --platformUrl "$JELASTIC_URL" > /dev/null
+/cli/jelastic/"$*" | sed '/./{H;$!d} ; x ; s/^[^{]*//g' | sed '/./{H;$!d} ; x ; s/[^}]*$//g' | jq
 )
 echo "$response"
