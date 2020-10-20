2e13a5e0545f1c7505b154b5dd28632ea5dafb68
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-10-20 11:44:29 +0300

    fix: Args propagation to entrypoint adjusted

--- entrypoint-github.sh
+++ entrypoint-github.sh
@@ -5,5 +5,5 @@ set -e
 export HOME=/cli  # To overwrite GitHub run command
 
 cd $HOME
-response=$(/cli/entrypoint.sh "$*" | jq --compact-output)
+response=$(/cli/entrypoint.sh "$1" "${@:2}" | jq --compact-output)
 echo "::set-output name=output::$response"
