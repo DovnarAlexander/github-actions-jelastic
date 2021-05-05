786924457844de29aebda558e6a39064fdf576af
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2021-05-05 16:08:24 +0300

    wip

--- files/entrypoints/entrypoint.sh
+++ files/entrypoints/entrypoint.sh
@@ -7,7 +7,7 @@ echo n | "$HOME"/jelastic/users/authentication/signin --login "$JELASTIC_USERNAM
 
 echo "Command execution..."
 env
-bash "$HOME/jelastic/$*" | tee "$OUTPUT"
+bash "$HOME"/jelastic/$* | tee "$OUTPUT"
 echo $?
 exit_code="$?"
 errors_found=$(grep -c "Error" "$OUTPUT" || echo 0)
