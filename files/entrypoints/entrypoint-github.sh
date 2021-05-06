e518ffc9110d2dffa07f273aad403376ec91139e
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2021-05-06 17:35:56 +0300

    test

--- files/entrypoints/entrypoint-github.sh
+++ files/entrypoints/entrypoint-github.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 
 set -e
 export HOME=/cli
 
-/bin/bash -c "set -e && "$HOME"/entrypoint.sh "$@""
+/bin/bash -c "set -e && $HOME/entrypoint.sh $@"
 echo "::set-output name=output::$(cat "$OUTPUT")"
